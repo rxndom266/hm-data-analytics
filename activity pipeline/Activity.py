7208e37ec737b5708942479afc2e9efab65e7505
Sharvani Vempati <73596860+Lucifer40402@users.noreply.github.com>
2021-07-08 22:42:17 +0530

    Update Activity.py

--- activity pipeline/Activity.py
+++ activity pipeline/Activity.py
@@ -295,7 +295,7 @@ class Activity:
       ]
     },
     "validate_url":{
-      "pattern":"/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/",
+      "pattern":"((http|https)://)(www.)?[a-zA-Z0-9@:%._\\+~#?&//=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%._\\+~#?&//=]*)",
       "fields":[
         "email_web_url",
         "email_clicked_through_url",
