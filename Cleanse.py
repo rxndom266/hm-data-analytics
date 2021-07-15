7a5799777199f1ddcfc662d7a5280abd4cca87f2
rxndom266 <59155793+rxndom266@users.noreply.github.com>
2021-07-15 22:13:19 +0530

    updated Cleanse.py

--- Cleanse.py
+++ Cleanse.py
@@ -9,99 +9,95 @@ class Cleanse:
   def __init__(self, config, load_type, inputdf, cust_code, errorlog = 'null'):
     self.config = config
     self.load_type = load_type
+    self.cust_code = cust_code
     self.schema = inputdf.schema
-    self.emptyCDM = spark.createDataFrame(emptyRDD, self.schema)
+    self.emptyCDM = spark.createDataFrame(self.emptyRDD, self.schema)
     if(errorlog=='null'):
       self.errorlog = self.emptyCDM.withColumn('error_type', lit(None))
     else:
       self.errorlog = errorlog
+    self.deltaPATH = '/delta/{}/{}/temp'.format(self.cust_code, self.load_type)
     
   def remove_null(self, df):
-    res = self.emptyCDM
     temp = df
     for k in self.config['mandatory']:
       err = temp.where(col(k).isNull())
       temp = temp.subtract(err)
       err = err.withColumn('error_type', lit("_".join(["MISSING", k.upper()])))
       self.errorlog = self.errorlog.union(err)
-    res = res.union(temp)
-    return res
+    return temp
+  
+  def optimise(self, temp):
+    df.write.format('delta').mode('overwrite').save(self.deltaPATH)
+    spark.sql("DROP TABLE  IF EXISTS temp")
+    spark.sql("CREATE TABLE temp USING DELTA LOCATION '{}'".format(self.deltaPATH))
+    spark.sql("OPTIMIZE temp ZORDER BY (base_activity_id)")
+    temp = spark.read.format('delta').load(self.deltaPATH)
+    return temp
   
   def validate_date(self, df):
     print('date validation')
-    res = self.emptyCDM
-    formats = self.config['date']['formats']
+    # formats = self.config['date']['formats']
     pattern = self.config['date']['pattern']
     temp = df
     for k in self.config['date']['fields']:
       valid = temp.where(col(k).isNull()) # not mandatory and null
-      check = temp.subtract(valid).withColumn(k, date_format(coalesce(*[to_date(k, f) for f in formats]), pattern)) # change format
-      err = check.where(col(k).isNull().withColumn('error_type', lit("_".join(["WRONG", k.upper(), "FORMAT"]))) # remove those with wrong format
-      self.errorlog = self.errorlog.union(err)
-      check = check.where(col(k).isNotNull())
-      err = check.where(col(k) < date_sub(current_date(), 3650) | col(k) > current_date()) # remove invalid values
-      check = check.subtract(err)
-      err = err.withColumn('error_type', lit("_".join(["INVALID", k.upper()])))
-      self.errorlog = self.errorlog.union(err)
-      temp = check.union(valid)
-      res = res.union(temp)
-    return res
-  
-  def validate_datetime(self, df):
-    print('date validation')
-    res = self.emptyCDM
-    formats = self.config['datetime']['formats']
-    pattern = self.config['datetime']['pattern']
-    temp = df
-    for k in self.config['datetime']['fields']:
-      valid = temp.where(col(k).isNull()) # not mandatory and null
-      check = temp.subtract(valid).withColumn(k, date_format(coalesce(*[to_date(k, f) for f in formats]), pattern)) # change format
+      res = res.union(valid)
+      check = temp.subtract(valid).withColumn(k, to_date(k, pattern)) # change format
       err = check.where(col(k).isNull()).withColumn('error_type', lit("_".join(["WRONG", k.upper(), "FORMAT"]))) # remove those with wrong format
       self.errorlog = self.errorlog.union(err)
       check = check.where(col(k).isNotNull())
-      err = check.where(col(k) < date_sub(current_date(), 3650) | col(k) > current_timestamp()) # remove invalid values
-      check = check.subtract(err)
-      err = err.withColumn('error_type', lit("_".join(["INVALID", k.upper()])))
-      self.errorlog = self.errorlog.union(err)
       temp = check.union(valid)
-      res = res.union(temp)
-    return res
+    return temp
+  
+  def validate_datetime(self, df):
+    print('datetime validation')    
+    # formats = self.config['datetime']['formats']
+    pattern = self.config['datetime']['pattern']
+    temp = df
+    for k in self.config['datetime']['fields']:
+      print(k)
+      valid = temp.where(col(k).isNull()) # not mandatory and null
+      check = temp.where(col(k).isNotNull()).withColumn(k, to_date(k, pattern)) # change format
+      err = check.where(col(k).isNull()).withColumn('error_type', lit("_".join(["WRONG", k.upper(), "FORMAT"]))) # remove those with wrong format
+      self.errorlog = self.errorlog.union(err)
+      print('added to errorlog')
+      check = check.where(col(k).isNotNull())
+      temp = check.union(valid)
+    return temp
   
   def validate_url(self, df):
     print('url validation')
-    res = self.emptyCDM
+    temp = df
     pattern = r'{}'.format(self.config['url']['pattern'])
     for k in self.config['url']['fields']:
       valid = temp.where(col(k).isNull() | (col(k).isNotNull() & col(k).rlike(pattern))) # not mandatory and null OR not null and valid
       err = temp.subtract(valid).withColumn('error_type', lit("_".join(["WRONG", k.upper(), "FORMAT"]))) # remove those with wrong format
-      self.error_df = self.error_df.union(err)
+      self.errorlog = self.errorlog.union(err)
       temp = valid
-    res = res.union(temp)
-    return res
+    return temp
   
   def validate_email(self, df):
     print('email validation')
-    res = self.emptyCDM
+    temp = df
     pattern = r'{}'.format(self.config['email']['pattern'])
     for k in self.config['email']['fields']:
       valid = temp.where(col(k).isNull() | (col(k).isNotNull() & col(k).rlike(pattern))) # not mandatory and null OR not null and valid
       err = temp.subtract(valid).withColumn('error_type', lit("_".join(["WRONG", k.upper(), "FORMAT"]))) # remove those with wrong format
-      self.error_df = self.error_df.union(err)
+      self.errorlog = self.errorlog.union(err)
       temp = valid
-    res = res.union(temp)
-    return res
+    return temp
   
-  def validate_email(self, df):
+  def validate_phone(self, df):
     print('phone validation')
-    res = self.emptyCDM
-    pattern = r'{}'.format(self.config['email']['pattern'])
+    pattern = r'{}'.format(self.config['phone']['pattern'])
     for k in self.config['phone']['fields']:
       valid = temp.where(col(k).isNull() | (col(k).isNotNull() & col(k).rlike(pattern))) # not mandatory and null OR not null and valid
       err = temp.subtract(valid).withColumn('error_type', lit("_".join(["WRONG", k.upper(), "FORMAT"]))) # remove those with wrong format
-      self.error_df = self.error_df.union(err)
+      self.errorlog = self.errorlog.union(err)
       temp = valid
-    res = res.union(temp)
-    return res
+    return temp
+  
   
   
   
@@ -110,14 +106,15 @@ activity_cleanse_config = {
     "base_activity_id"
   ],
   "date":{
-    "formats":["yyyy-MM-dd", "yyyy/MM/dd", "MM/dd/yyyy", "dd/MM/yyyy", "dd-MM-yyyy"],
+    "formats":["yyyy-MM-dd", "dd-MM-yyyy"],
     "pattern":"yyyy-MM-dd",
     "fields":[
-      
+      "birth_date"
     ]
   },
   "datetime":{
-    "formats":["yyyy-MM-dd HH:mm:ss", "yyyy/MM/dd HH:mm:ss", "MM/dd/yyyy HH:mm:ss", "dd/MM/yyyy HH:mm:ss", "dd-MM-yyyy HH:mm:ss", "HH:mm:ss yyyy-MM-dd", "HH:mm:ss yyyy/MM/dd", "HH:mm:ss MM/dd/yyyy", "HH:mm:ss dd/MM/yyyy", "HH:mm:ss dd-MM-yyyy"],
+    "formats":["yyyy-MM-dd HH:mm:ss", "dd-MM-yyyy HH:mm:ss"],
+    "format":["yyyy-MM-dd HH:mm:ss", "yyyy/MM/dd HH:mm:ss", "MM/dd/yyyy HH:mm:ss", "dd/MM/yyyy HH:mm:ss", "dd-MM-yyyy HH:mm:ss", "HH:mm:ss yyyy-MM-dd", "HH:mm:ss yyyy/MM/dd", "HH:mm:ss MM/dd/yyyy", "HH:mm:ss dd/MM/yyyy", "HH:mm:ss dd-MM-yyyy"],
     "pattern":"yyyy-MM-dd HH:mm:ss",
     "fields":[
       "email_sent_datetime",
