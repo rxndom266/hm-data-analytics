0ee1fd4f445d2b1df84ed55b0550648faec2a531
rxndom266 <59155793+rxndom266@users.noreply.github.com>
2021-07-23 14:28:32 +0530

    Add files via upload

---
+++ FINAL/person/person/Person.py
@@ -0,0 +1,35 @@
+def driver(config):
+    client_code = config['client_code']
+    data_source = config['data_source']
+    load_type = config['load_type']
+    load_id = config['load_id']
+    enrich_flag = config['enrich_flag']
+    batch_datetime = config['batch_datetime']
+    mount_root = config['mount_root']
+    client_data_path = config['client_data_path']
+    file_name = config['file_name']
+    load_id = config['load_id']
+    client_file_path = client_data_path+'/'+file_name
+    transpath = '/delta/{}/{}/{}/transform'.format(client_code, data_source, load_type)
+    cleanpath = '/delta/{}/{}/{}/clean'.format(client_code, data_source, load_type)
+    rejectpath = '/delta/{}/{}/{}/reject'.format(client_code, data_source, load_type)
+    idpath = '/delta/{}/{}/id'.format(client_code, data_source)
+    enrichpath = '/delta/{}/person/enriched'.format(client_code, data_source)
+    cdmpath = '/delta/{}/person/cdm'.format(client_code)
+    
+    
+    from transform import Transform
+    Transform.transform(load_id, load_type, data_source, client_file_path, transpath)
+    
+    from cleanse import Cleanse
+    Cleanse.cleanse(load_id, load_type, transpath, cleanpath, rejectpath)
+    
+    from person.identify import identify
+    identify(load_id, cleanpath, idpath, cdmpath)
+
+    if enrich_flag==1:
+        from person.enrich import enrich
+        enrich(cdmpath, enrichpath)
+
+    return {"transform":transpath, "cleanse":cleanpath, "identify":idpath, "reject":rejectpath, "cdm":cdmpath, "enrich":enrichpath}
+
