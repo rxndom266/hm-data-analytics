48262c5890b278736c95cabb78fcdd215fcf856d
rxndom266 <59155793+rxndom266@users.noreply.github.com>
2021-07-23 15:40:17 +0530

    Add files via upload

---
+++ FINAL/person/person/enrich.py
@@ -0,0 +1,20 @@
+from pyspark.sql.functions import col, lit, when
+from delta.tables import *
+
+spark = SparkSession \
+    .builder \
+    .appName("Enrich") \
+    .config("spark.sql.legacy.timeParserPolicy", "LEGACY") \
+    .getOrCreate()
+def enrich(load_id, srcpath, destpath):
+    null='--'
+    dbutils.fs.rm(destpath, recurse=True)
+    data = spark.read.format('delta').load(srcpath).where(col('delete_flag')==0)
+    data = data.withColumn('age_group', when(lit((col('age')>=0) & (col('age')<=14)), 'child')\
+                                        .when(lit((col('age')>14) & (col('age')<=24)), 'youth')\
+                                        .when(lit((col('age')>24) & (col('age')<=64)), 'adult')\
+                                        .when(lit(col('age')>64), 'senior'))
+    
+    data.write.format('delta').save(destpath)
+    print('enrich done')
+    return
